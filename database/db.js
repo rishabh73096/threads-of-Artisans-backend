763a2ec16224660f95b472ce24bf7bbed5dcadfc
Rishabh tiwari <rishabhtiwari73096@gmail.com>
2024-09-16 20:57:23 +0530

    third commit

--- database/db.js
+++ database/db.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
-const dbURL = "mongodb+srv://tiwari7309651657:Rishabh%407309651657@cluster0.x0yhjfd.mongodb.net/threads_of_artisans_db";
+const dbURL = "mongodb://tiwari7309651657:Rishabh%407309651657@cluster0-shard-00-00.un6p7.mongodb.net:27017,cluster0-shard-00-01.un6p7.mongodb.net:27017,cluster0-shard-00-02.un6p7.mongodb.net:27017/?replicaSet=atlas-ifrupx-shard-0&ssl=true&authSource=admin";
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(dbURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000 // 30 seconds
-          });
+        const connection = await mongoose.connect(dbURL);
         console.log("Connected to MongoDB");    
        
     } catch (error) {
